20ae2011a6aa00cc3b106c2da38311b001d276cf
stavbers <reddevweb@gmail.com>
2023-04-10 15:38:15 +0300

    some js fix

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-const accordEl = document.querySelectorAll('.accordion-body__elm')
-const accordLinks = document.querySelectorAll('.accordion__item')
-
-
-function accordion(){
-  accordLinks.forEach((el, indx)=> {
-    
-    el.addEventListener('click', ()=> {
-      hideItem(accordEl)
-      showItem(accordEl[indx])
-    })
-  })
-}
-
-
-
-function showItem(el){
-  el.classList.remove('accordion-hide')
-}
-
-function hideItem(el){
-el.forEach(element => {
-  element.classList.add('accordion-hide')
+document.addEventListener("DOMContentLoaded", function(event){
+  const accordEl = document.querySelectorAll('.accordion-body__elm')
+  const accordLinks = document.querySelectorAll('.accordion__item')
   
+  function accordion(){
+    accordLinks.forEach((el, indx)=> {
+      
+      el.addEventListener('click', ()=> {
+        hideItem(accordEl)
+        showItem(accordEl[indx])
+      })
+    })
+  }
+  
+  function showItem(el){
+    el.classList.remove('accordion-hide')
+  }
+  
+  function hideItem(el){
+  el.forEach(element => {
+    element.classList.add('accordion-hide')
+    
+  });
+  }
+
+  accordion()
 });
-}
-
-// hideItem(accordEl)
-// showItem(accordEl[1])
-
-accordion()
+  
